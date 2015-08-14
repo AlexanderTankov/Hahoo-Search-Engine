3e1d7f07cd2fba10735a4a8791c0a328821a9491
Alexander Tankov <alexandartankov@gmail.com>
2015-08-14 21:29:25 +0300

    Merge Hahoo.cabal

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,3 @@
+import Persister
+
 main = undefined
--- src/Persister.hs
+++ src/Persister.hs
@@ -1,7 +1,22 @@
- import qualified Data.Map.Strict as M
+module Persister where
 
- write :: M.Map -> IO()
- write = undefined
+import qualified Data.Map.Strict as M
+import qualified Data.ByteString.Lazy as BL
 
- read :: IO(M.Map)
- read = undefined
+-- Path to file
+getFilePath :: String
+getFilePath = "/home/alexandar/Documents/Haskell/Week7/Hahoo/src"
+
+fromMapToByteString :: Ord k => M.Map k a -> [(k, a)]
+fromMapToByteString m = M.toList m
+
+fromByteStringToMap :: Ord k => [(k, a)] -> M.Map k a
+fromByteStringToMap l = M.fromList l
+
+writeInFile :: (Ord k, Show a, Show k) => M.Map k a -> IO()
+writeInFile m = writeFile getFilePath $ show $ fromMapToByteString m
+
+readFromFile :: (Ord k, Read a, Read k) => IO(M.Map k a)
+readFromFile = do
+    m <- readFile getFilePath
+    return $ fromByteStringToMap $ read m
